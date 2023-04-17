87486f692f339fd3e758db229b183cb5e151522f
SunilKrishnaKumar <komadamsunil96@gmail.com>
2023-04-17 02:44:51 -0500

    RestAPI + Viewset + Pagination

--- project/weatherData/urls.py
+++ project/weatherData/urls.py
@@ -1,7 +1,21 @@
-from django.urls import path
-from .views import getWeather, getWeatherStats
+## Basic URL Configurations
+from unicodedata import name
+from django.urls import path, include
+## Import routers
+from rest_framework import routers
+## Import everything from views
+from .views import *
 
+## Define Router
+router = routers.DefaultRouter()
+
+## Define router paths and viewset to be used
+
+router.register(r'api/weather', WeatherViewSet, basename='weather-data')
+router.register(r'api/weather/stats', WeatherStatsViewSet, basename='weather-stats')
+
+## specify URL path for rest_framework
 urlpatterns = [
-    path('', getWeather),
-    path('stats/', getWeatherStats)
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls'))
 ]
